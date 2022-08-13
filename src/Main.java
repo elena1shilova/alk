855ad4da754bc9f61725900c34e41994b47800ec
elena1989 <elena.shilova89@gmail.com>
2022-08-13 16:23:08 +0300

    Создала метод calk, который принимает строку

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите данные для вычисления через пробел: ");
+        String newCAlk = calc(s.nextLine());
+    }
+
+
+    public static String calc(String newCAlk){
+        String [] newStr = newCAlk.split(" ");//убираем пробелы и кладем в новый массив.
+
+        for (String b: newStr) {
+            System.out.print(b);
+        }
+        return newCAlk;
+    }
 }
